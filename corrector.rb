648666970507d44951dbc9feadd736ec45e3691c
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-09 18:40:54 +0500

    Create class for corrector method to modify name

---
+++ corrector.rb
@@ -0,0 +1,16 @@
+class Corrector
+  attr_accessor :fullname
+
+  def initialize(fullname)
+    @fullname = fullname
+  end
+
+  def correct_name
+    if fullname.length > 10
+      fullname[0..10].split.map(&:capitalize).join(' ')
+
+    else
+      fullname.split.map(&:capitalize).join(' ')
+    end
+  end
+end
