aac00a7e9108e2f56a614d1b93882781a2a5cc44
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-09 19:37:05 +0500

    use instance variables to call

--- person.rb
+++ person.rb
@@ -2,18 +2,18 @@ require './corrector'
 
 class Person
   attr_accessor :name, :age, :permission
-  attr_reader :id
+  attr_reader :id, :corrector
 
   def initialize(age, name, permission: true)
     @id = Random.rand(1..1000)
     @name = name
-    @corrector = Corrector.new(name)
+    @corrector = Corrector.new(@name)
     @age = age
     @permission = permission
   end
 
   def validate_name
-    @name = corrector.correct_name
+    @name = @corrector.correct_name
   end
 
   def can_use_service?
