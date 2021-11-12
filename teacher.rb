bfe2945ba3320d9cab9895f3fe20f6e38771a98c
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-13 03:06:58 +0500

    Create executable file to run the program for school library

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown')
+  attr_accessor :type, :specialization
+
+  def initialize(specialization, age, name = 'Unknown', type = 'teacher')
     super(name, age)
     @specialization = specialization
+    @type = type
   end
 
   def can_use_service?
